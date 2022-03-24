98a862e77506c92bb8c1d5b022761546baf4688f
daylim95 <daylim95@gmail.com>
2022-03-24 23:02:24 +0900

    Create Sol2606.java

---
+++ Sol2606.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Sol2606{
+  
+  private int node;
+  private Map<Integer, Integer> edges = new HashMap<>();
+
+  private void dfs(){
+
+  }
+
+  public static void main(String[] args){
+
+    System.in()
+
+  } 
+}
