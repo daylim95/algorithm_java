d502b5757b8020e0d034108a8dc741a69a1f2654
daylim95 <daylim95@gmail.com>
2022-03-31 23:15:47 +0900

    Create Sol1000.java, Sol1001.java

---
+++ Sol1001.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Sol1001 {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int m = scan.nextInt();
+    int n = scan.nextInt();
+    scan.close();
+    System.out.println(m+n);
+  }
+}
