38e7a575faddf8c22a966ffcc8289dd17b1a318f
daylim95 <daylim95@gmail.com>
2022-03-31 23:09:15 +0900

    Update Sol2606.java

--- Sol2606.java
+++ Sol2606.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Sol2606{
   
-  private int node;
-  private Map<Integer, Integer> edges = new HashMap<>();
+  private static int node;
+  private static int edge;
+  private static Map<Integer, Integer> edges = new HashMap<>();
+  private static ArrayList<Integer[]> graph = null;
+  
+  private static void dfs(){
+    
+    boolean[] visited = new boolean[node];
+    for(int i=0;i<node;i++){
+      visited[i] = false;
+    }
 
-  private void dfs(){
+    int infected = 0;
+
 
   }
 
   public static void main(String[] args){
 
-    System.in()
-
+    Scanner scan = new Scanner(System.in);
+    node = scan.nextInt();
+    if(node<101){
+      edge = scan.nextInt();
+      for(int i=0;i<edge;i++){
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        edges.put(m, n);
+      }
+      scan.close();
+      dfs();
+    }else{
+      System.out.println("컴퓨터의 수는 100대 이하");
+    }
   } 
 }
